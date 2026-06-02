61a6dfa56317cd9c8199f86d3479434265dceadb
Felipe Kauãn <felipekauan30@outlook.com>
2026-06-02 19:41:44 -0300

    feat: create EnsureTokenIsValid middleware and register alias

---
+++ app/Http/Middleware/EnsureTokenIsValid.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Http;
+
+class EnsureTokenIsValid
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $token = $request->bearerToken();
+
+        if (!$token) {
+            return response()->json(['message' => 'Token não enviado.'], 401);
+        }
+
+        $response = Http::withToken($token)->get('http://localhost:8000/api/validate-token');
+
+        if ($response->successful()) {
+            return $next($request);
+        }
+
+        return response()->json(['message' => 'Token inválido.'], 401);
+    }
+}
